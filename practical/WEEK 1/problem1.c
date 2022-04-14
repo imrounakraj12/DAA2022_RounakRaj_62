24a1f04ff09716501ebf8761328c5dcc4ae4c3af
imrounakraj12 <102277360+imrounakraj12@users.noreply.github.com>
2022-04-14 14:44:00 +0530

    Update problem1.c

--- practical/WEEK 1/problem1.c
+++ practical/WEEK 1/problem1.c
@@ -1,1 +1,37 @@
+#include <stdio.h>
+#define max 100
+int main()
+{
+    int arr[max];
+    int n;
+    int key;
+    int i ,found;
+    printf("Enter the number of elements in a array:");
+    scanf("%d",&n);
+    printf("Enter the elements of the array:");
+    for( i =0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    printf("Enter the value of the key:");
+    scanf("%d",&key);
+    found = 0;
+    for( i =0;i<n;i++)
+    {
+        if(arr[i]==key)
+        {
+            found = 1;
+            break;
+        }
+    }
+    if(found == 1)
+    {
+        printf("\n%d is found at position %d", key, i + 1);
+    }
+    else
+    {
+        printf("\n%d is not found in the array", key);
+    }
 
+    return 0;
+}
